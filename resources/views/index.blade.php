7388d1a0819546047f3c66c3c8b2971627419d30
unknown <zhack001@gmail.com>
2021-01-03 23:12:46 +0800

    lechonsUpdate0103

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -316,7 +316,50 @@
 		</div>
 		</div>
 		<div id="footer" class="footer-section">
-			<p>My footer</p>
+			<div class="footer-container">
+				<div class="footer-company">
+					<div class="footer-logo">
+						<span class="des-logo">
+							<img src="images/logo.png" class="footer-logos">
+						</span>
+						<div class="name-title">
+							<span class="des1">Karne De Lame</span>
+							<div class="des2">Fresh Meats</div>
+						</div>
+					</div>
+					<div class="footer-info">
+						<div class="info-paragraph">
+							New to the store are our bags of mixed dried berries. A perfect healthy snack or a nice topping for your cereal in the morning. We are a specialist food retailer and we have
+						</div>
+					</div>
+				</div>
+				<div class="footer-tweets">
+					<div class="footer-title-tweets">
+						<span class="footer-title">Latest Tweets</span>
+					</div>
+					<div class="footer-tweet-time">
+						<span><i>30 minutes ago</i></span>
+					</div>
+				</div>
+				<div class="footer-gallery">
+					<div>
+						<span class="footer-title">Restaurant Gallery</span>
+					</div>					
+				</div>
+				<div class="footer-contact-us">
+					<div class="footer-contact">
+						<span class="footer-title">Contact Us</span>
+					</div>
+					<div class="footer-contact-info">
+						<span><b>Tel :</b>	+123456789</span>
+						<span><b>Phone :</b>	987654321</span>
+						<span><b>Fax :</b>	5455555</span>
+						<span><b>Email :</b>	info@yourdomain.com</span>
+						<span><b>Address :</b>	12453 Avenue NYC, New York City, NYC, 98741</span>
+						<span><b>Website :</b>	http://themexlab.com</span>
+					</div>					
+				</div>
+			</div>
 		</div>
 	</div>
 	 	<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.js"></script>
