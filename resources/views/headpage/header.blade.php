e7649047bc9db2392cb01a87b3206548425af4f8
elvis <elvis.pionita@gmail.com>
2021-01-03 23:25:14 +0800

    updated

---
+++ resources/views/headpage/header.blade.php
@@ -0,0 +1,53 @@
+<div class="headmenu">
+		<span class="des-logo">
+			<img src="images/logo.png" class="logos">
+		</span>
+		<span class="des1">Karne De Lame</span>
+		<div class="des2">Fresh Meats</div>
+		<span class="sub-menu">
+			<a href="{{asset('frontpage/shop')}}">
+			<span class="des3">Shop</span>
+			</a>
+			<span class="dropdown">
+				<span class="des4">Fish <img src="images/down-arrow.png" class="d-arrow"></span>
+				<div class="dropdown-content">
+				  <a href="#">Sea Food</a>
+				</div>
+			</span>
+
+			<span class="dropdown">
+				<span class="des5">Meat <img src="images/down-arrow.png" class="d-arrow"></span>
+				<div class="dropdown-content">
+				  <a href="#">Rabbit</a>
+				  <a href="#">Beef</a>
+				  <a href="#">Pork</a>
+				  <a href="#">Chicken</a>
+				  <a href="#">Lamb</a>
+				</div>
+			</span>
+			
+			<span class="des6">About Us <img src="images/down-arrow.png" class="d-arrow"></span>
+			<span class="des7">Blocks <img src="images/down-arrow.png" class="d-arrow"></span>
+		</span>
+		<span class="inputs">
+			<input type="text" name="" placeholder="Search products..." class="inpt-search">
+			<button class="btn-search">Search</button>
+		</span>
+		<div class="icons">
+			
+			<span>
+				<img src="images/cart.png" class="icons3" title="Your Cart">
+			</span>
+			<div class="dropdown">
+			<span class="account-c ">
+				<img src="images/user.png" class="icons2">
+
+				<div class="dropdown-content">
+					<a href="{{asset('/login')}}">Login</a>
+   				 	<a href="{{asset('/register')}}">Register</a>
+   
+ 				</div>
+			</span>
+			</div>
+		</div>
+	</div>
