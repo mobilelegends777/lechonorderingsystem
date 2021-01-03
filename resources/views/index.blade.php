abc2b4634e257d00917a7420c87c6d08d2eee603
unknown <zhack001@gmail.com>
2021-01-04 05:41:19 +0800

    frontPage Footer0104

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -326,7 +326,7 @@
 		</div>
 		<div id="footer" class="footer-section">
 			<div class="footer-container">
-				<div class="footer-company">
+				<div class="footer-desc footer-company">
 					<div class="footer-logo">
 						<span class="des-logo">
 							<img src="images/logo.png" class="footer-logos">
@@ -338,39 +338,75 @@
 					</div>
 					<div class="footer-info">
 						<div class="info-paragraph">
-							New to the store are our bags of mixed dried berries. A perfect healthy snack or a nice topping for your cereal in the morning. We are a specialist food retailer and we have
+							New to the store are our best Lechons. A perfect healthy snack or a nice topping for your cereal in the morning. We are a specialist food retailer and we have
 						</div>
 					</div>
 				</div>
-				<div class="footer-tweets">
+				<div class="footer-desc footer-tweets">
 					<div class="footer-title-tweets">
 						<span class="footer-title">Latest Tweets</span>
 					</div>
 					<div class="footer-tweet-time">
-						<span><i>30 minutes ago</i></span>
+						<span><img src="images/twits.png" class="tweets"><i>30 minutes ago</i></span>
 					</div>
 				</div>
-				<div class="footer-gallery">
-					<div>
+				<div class="footer-desc footer-gallery">
+					<div class="footer-title-gallery">
 						<span class="footer-title">Restaurant Gallery</span>
+					</div>
+					<div class="image-cont">
+						<!-- append -->
 					</div>					
 				</div>
-				<div class="footer-contact-us">
-					<div class="footer-contact">
+				<div class="footer-desc footer-contact-us">
+					<div class="footer-title-contact">
 						<span class="footer-title">Contact Us</span>
 					</div>
 					<div class="footer-contact-info">
-						<span><b>Tel :</b>	+123456789</span>
-						<span><b>Phone :</b>	987654321</span>
-						<span><b>Fax :</b>	5455555</span>
-						<span><b>Email :</b>	info@yourdomain.com</span>
-						<span><b>Address :</b>	12453 Avenue NYC, New York City, NYC, 98741</span>
-						<span><b>Website :</b>	http://themexlab.com</span>
+						<div class="info-tel">
+							<b class="info-contacts">Tel :</b>
+							<span class="sp">+123456789</span>
+						</div>
+						<div class="info-phone">
+							<b class="info-contacts">Phone :</b>
+							<span class="sp">987654321</span>
+						</div>
+						<div class="info-fax">
+							<b class="info-contacts">Fax :</b>
+							<span class="sp">5455555</span>
+						</div>	
+						<div class="info-mail">
+							<b class="info-contacts">Email :</b>
+							<span class="sp">info@yourdomain.com</span>
+						</div>
+						<div class="info-address">
+							<b class="info-contacts address">Address :</b>
+							<span class="sp">12453 Avenue NYC, New York City, NYC, 98741</span>
+						</div>	
+						<div class="info-web">
+							<b class="info-contacts">Website :</b>
+							<span class="sp">http://themexlab.com</span>
+						</div>
 					</div>					
 				</div>
 			</div>
+			<div class="footers-copyright">
+				<!-- <p>myFooter</p> -->
+				<div class="copyright">
+					<span>Copyright 2021 by Buffalo | All rights reserved</span>
+				</div>
+				<div class="social-media-link">
+					<ul class="social-icons">
+						<!-- append -->
+					</ul>
+				</div>
+				<div class="back-to-top">
+					<button id="Btop">
+						<img src="images/btop.png">
+					</button>
+				</div>
+			</div>
 		</div>
-	</div>
 	 	<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.js"></script>
     	<script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
 </body>
@@ -455,6 +491,43 @@ $('.slider').slick({
 //         $('.items-price').css("background", "#f6f4f2");
 //     });
 // });
+
+$(document).ready(function(){
+	for (var i = 1;i<13;i++){
+		$('.image-cont').append(`
+			<div class="gallery-images">
+				<img src="images/image${i}.jpg">
+			</div>
+		`);
+
+
+	}
+	for (var x = 1; x < 5; x++){
+		$('.social-icons').append(`
+			<li class="social-media-icons"><a href="#"><img src="images/social${x}.png"></a></li>
+		`);
+	}
+	setInterval(function(){
+     $(".footer-tweet-time").toggle().slideDown();
+    // $("#box1").toggle();
+},3000);
+
+var btn = $('#Btop');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 600) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
+});
 
 </script>
 
