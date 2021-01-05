3fb28593bfc07d9f0677b1485682d7f744d5dc09
unknown <zhack001@gmail.com>
2021-01-06 06:02:25 +0800

    update010621

--- resources/views/layouts/scripts.blade.php
+++ resources/views/layouts/scripts.blade.php
@@ -130,6 +130,16 @@ $(document).ready(function(){
   }, 5000);
 });
 
+// onscroll change image size
+// $(window).scroll(function() {
+//   var scroll = $(window).scrollTop();
+//   $(".slider-conts img").css({
+//     transform: 'translate3d(-50%, -'+(scroll/100)+'%, 0) scale('+(100 + scroll/5)/100+')',
+    //Blur suggestion from @janwagner: https://codepen.io/janwagner/ in comments
+    //"-webkit-filter": "blur(" + (scroll/200) + "px)",
+    //filter: "blur(" + (scroll/200) + "px)"
+//   });
+// });
 </script>
 
 
