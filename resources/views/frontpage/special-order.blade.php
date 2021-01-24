f0177c49308b164ff7679b02150f6dc26181af6d
unknown <zhack001@gmail.com>
2021-01-24 19:08:03 +0800

    update012421

--- resources/views/frontpage/special-order.blade.php
+++ resources/views/frontpage/special-order.blade.php
@@ -10,8 +10,68 @@
 				</span>
 			</div>
 			<div class="form-container">
-				<div>
-										
+				<div class="order-conts">
+						<span><h4>ORDER NOW</h4></span>	
+							<div>
+								<span>How do you want your order?</span>
+							</div>
+							<div class="radio-buttons">
+								<div class="radio1">
+									<input type="radio" name="orderType"> Pick-up
+								</div>
+								<div class="radio2">
+									<input type="radio" name="orderType"> Delivery
+								</div>
+							</div>
+							<div>
+								<span>Date/Time</span>
+							</div>
+							<div class="date-time">
+								<div class="date-when">
+									<input type="date" name="">
+								</div>
+								<div class="oras">
+									<select class="hour-when">
+										<option>1</option>
+										<option>2</option>
+										<option>3</option>
+										<option>4</option>
+										<option>5</option>
+										<option>6</option>
+										<option>7</option>
+										<option>8</option>
+										<option>9</option>
+										<option>10</option>
+										<option>11</option>
+										<option>12</option>
+									</select>
+								</div>
+								<div class="minoto">
+									<select class="minute-when">
+										<option>00</option>
+										<option>15</option>
+										<option>30</option>
+										<option>45</option>
+									</select>
+								</div>
+								<div class="adlaw">
+									<select>
+										<option>AM</option>
+										<option>PM</option>
+									</select>
+								</div>
+							</div>
+							<div class="pick-up-add">
+								<div>
+									<span>Address</span>
+								</div>
+								<div class="pickup-deliv">
+									<input type="addresses" name="">
+								</div>
+								<div class="proceed-to">
+									<button><i class="fa fa-arrow-right">PROCEED</i></button>
+								</div>
+							</div>
 				</div>
 			</div>
 	</div>
